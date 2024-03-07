a1ff7a478a7c033a4ce66d4109b6ba30d9e585a1
malekba <malek.alaya@tum.de>
2024-03-08 00:04:57 +0100

    Add filename to Atlas_over_100.csv for masks with over 100 pixels

--- core/Atlas_preprocessing.py
+++ core/Atlas_preprocessing.py
@@ -5,7 +5,8 @@ mask_dir = ['./data/ATLAS/splits/atlas_train_mask_png.csv']
 from torch.utils.data import DataLoader
 
 from data.loaders.ixi_loader import mask_preprocessing_loader
-
+import csv
+PIXELS_THRESHOLD = 100
 dataset = mask_preprocessing_loader(image_dir, label_dir=mask_dir, target_size=(128, 128), test=False)
 loader = DataLoader(
             dataset,
@@ -19,11 +20,17 @@ sum = 0
 for data in loader:
     image = data[0].to(device)
     mask = data[1].to(device)
+    index = data[2].to(device)
+    filename = data[3]
     non_zero_pixels = mask.sum()
-    if non_zero_pixels.item() > 100:
+    if non_zero_pixels.item() >= PIXELS_THRESHOLD:
+        print("filename", filename[0])
+        with open('Atlas_over_100.csv', 'a', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow([filename[0]])
         sum += 1
     #print(image.shape, mask.shape)
-    index = data[2].to(device)
+    
     #print(index)
 
 print("number of over 100 pixels", sum)
