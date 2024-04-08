62219159a7443cdcc50050767a4d09c9bec4040a
{malekba98} <malek.alaya@tum.de>
2024-04-08 03:50:55 +0200

    add test dataset preparation nnunet

--- core/nnunet_atlas_dataset_prepatation.py
+++ core/nnunet_atlas_dataset_prepatation.py
@@ -15,47 +15,66 @@ import shutil
 from PIL import Image 
 import numpy as np
 
-PIXELS_THRESHOLD = 0
-dataset = mask_preprocessing_loader(
+PREPROCESS_TRAIN = False
+
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+if PREPROCESS_TRAIN:
+    dataset = mask_preprocessing_loader(
     image_dir,
     label_dir=label_dir,
     mask_dir=mask_dir,
     target_size=(128, 128),
     test=False,
-)
-loader = DataLoader(
-    dataset, batch_size=1, shuffle=False, drop_last=False, pin_memory=True
-)
+    )
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
-sum = 0
-if os.path.exists(f"Atlas_over_{PIXELS_THRESHOLD}.csv"):
-    os.remove(f"Atlas_over_{PIXELS_THRESHOLD}.csv")
+    loader = DataLoader(
+        dataset, batch_size=1, shuffle=False, drop_last=False, pin_memory=True
+    )
 
-if os.path.exists(f"Atlas_mask_over_{PIXELS_THRESHOLD}.csv"):
-    os.remove(f"Atlas_mask_over_{PIXELS_THRESHOLD}.csv")
+    for data in loader:
+        image = data[0].to(device)
+        mask = data[1].to(device)
+        idx = data[2]
+        filename = data[3]
+        mask_filename = data[4]
+        # copy file with filename to new directory "./nnunet_data/nnunet_raw/Dataset500_ATLAS/imagesTr"
+        # copy file with mask_filename to new directory "./nnunet_data/nnunet_raw/Dataset500_ATLAS/labelsTr"
+        # ATLAS_001_0000.png
+        image_dest = f"./nnunet_data/nnunet_raw/Dataset500_ATLAS/imagesTr/ATLAS_{int(idx):03d}_0000.png"
+        mask_dest = f"./nnunet_data/nnunet_raw/Dataset500_ATLAS/labelsTr/ATLAS_{int(idx):03d}.png"
+        shutil.copy(filename[0], image_dest)
+        #shutil.copy(mask_filename[0], mask_dest)
 
-if os.path.exists(f"Atlas_brain_mask_over_{PIXELS_THRESHOLD}.csv"):
-    os.remove(f"Atlas_brain_mask_over_{PIXELS_THRESHOLD}.csv")
+        mask_array = np.array(Image.open(mask_filename[0]))
 
-for data in loader:
-    image = data[0].to(device)
-    mask = data[1].to(device)
-    idx = data[2]
-    filename = data[3]
-    mask_filename = data[4]
-    # copy file with filename to new directory "./nnunet_data/nnunet_raw/Dataset500_ATLAS/imagesTr"
-    # copy file with mask_filename to new directory "./nnunet_data/nnunet_raw/Dataset500_ATLAS/labelsTr"
-    # ATLAS_001_0000.png
-    image_dest = f"./nnunet_data/nnunet_raw/Dataset500_ATLAS/imagesTr/ATLAS_{int(idx):03d}_0000.png"
-    mask_dest = f"./nnunet_data/nnunet_raw/Dataset500_ATLAS/labelsTr/ATLAS_{int(idx):03d}.png"
-    shutil.copy(filename[0], image_dest)
-    #shutil.copy(mask_filename[0], mask_dest)
+        # Convert 255 to 1
+        mask_array[mask_array == 255] = 1
 
-    mask_array = np.array(Image.open(mask_filename[0]))
+        # Save the converted mask back to a file
+        Image.fromarray(mask_array).save(mask_dest)
+else:
+    image_dir = ["./data/ATLAS/splits/atlas_test_png.csv"]
+    label_dir = ["./data/ATLAS/splits/atlas_test_png.csv"]
 
-    # Convert 255 to 1
-    mask_array[mask_array == 255] = 1
+    mask_dir = ["./data/ATLAS/splits/atlas_test_png.csv"]
 
-    # Save the converted mask back to a file
-    Image.fromarray(mask_array).save(mask_dest)
+    dataset = mask_preprocessing_loader(
+    image_dir,
+    label_dir=label_dir,
+    mask_dir=mask_dir,
+    target_size=(128, 128),
+    test=False,
+    )
+
+    loader = DataLoader(
+        dataset, batch_size=1, shuffle=False, drop_last=False, pin_memory=True
+    )
+
+    for data in loader:
+        filename = data[3]
+        idx = data[2]
+
+        image_dest = f"./nnunet_data/nnunet_raw/Dataset500_ATLAS/imagesTs/ATLAS_{int(idx):03d}_0000.png"
+        shutil.copy(filename[0], image_dest)
