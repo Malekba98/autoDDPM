3a67c9db95b5ca0ed8ac5db14882232aebbb0421
{malekba98} <malek.alaya@tum.de>
2024-04-10 21:15:31 +0200

    Remove set_env.sh file

--- set_env.sh
+++
@@ -1,3 +0,0 @@
-export nnUNet_raw="/home/malek/autoDDPM/nnunet_data/nnunet_raw"
-export nnUNet_preprocessed="/home/malek/autoDDPM/nn_unet_data/nnunet_preprocessed"
-export nnUNet_results="/home/malek/autoDDPM/nnunet_data/nnunet_results"
