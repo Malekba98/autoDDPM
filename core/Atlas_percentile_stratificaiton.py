5e01b39d0a9d3cc1aeaf8f1c14e9fcc61f5d0f0b
Malek Ben Alaya <malek.alaya@tum.de>
2024-05-06 18:33:21 +0200

    Add train-test split and save files for Atlas_percentile_stratificaiton.py

--- core/Atlas_percentile_stratificaiton.py
+++ core/Atlas_percentile_stratificaiton.py
@@ -1,4 +1,5 @@
 import torch
+from sklearn.model_selection import train_test_split
 
 image_dir = ["./data/ATLAS/splits/atlas_train_png.csv"]
 label_dir = ["./data/ATLAS/splits/atlas_train_mask_png.csv"]
@@ -62,6 +63,11 @@ print("Between 25 and 75 percentile:", between_25_and_75)
 
 
 labels = []
+file_paths = []
+mask_file_paths = []
+brain_mask_file_paths = []
+
+
 for data in loader:
     image = data[0].to(device)
     mask = data[1].to(device)
@@ -78,6 +84,12 @@ for data in loader:
     else:
         labels.append("medium")
 
+    file_paths.append([filename[0]])
+    mask_file_paths.append([mask_filename[0]])
+    brain_mask_file_paths.append([brain_mask_filename[0]])
+
+    
+
 
 high_count = labels.count("high")
 low_count = labels.count("low")
@@ -89,3 +101,86 @@ print("Number of medium:", medium_count)
 
 
 print("number of cases", len(labels))
+
+
+# Split data into train and temp sets
+(
+    train_files,
+    val_files,
+    train_labels,
+    val_labels,
+    train_mask_files,
+    val_mask_files,
+    train_brain_mask_files,
+    val_brain_mask_files,
+) = train_test_split(
+    file_paths,
+    labels,
+    mask_file_paths,
+    brain_mask_file_paths,
+    test_size=0.23,
+    stratify=labels,
+)
+
+
+print("train files", train_files)
+
+train_file = "atlas_train_png.csv"
+val_file = "atlas_val_png.csv"
+
+output_folder = f"data/ATLAS/splits_over_{PIXELS_THRESHOLD}_stratified"
+os.makedirs(output_folder, exist_ok=True)
+
+train_file_path = os.path.join(output_folder, train_file)
+val_file_path = os.path.join(output_folder, val_file)
+
+
+with open(train_file_path, "w", newline="") as file:
+    writer = csv.writer(file)
+    writer.writerows(train_files)
+
+with open(val_file_path, "w", newline="") as file:
+    writer = csv.writer(file)
+    writer.writerows(val_files)
+
+print("Train file saved at:", train_file_path)
+print("Validation file saved at:", val_file_path)
+
+# DO SAME FOR MASKS
+
+train_mask_file = "atlas_train_mask_png.csv"
+val_mask_file = "atlas_val_mask_png.csv"
+
+train_file_path = os.path.join(output_folder, train_mask_file)
+val_file_path = os.path.join(output_folder, val_mask_file)
+
+with open(train_file_path, "w", newline="") as file:
+    writer = csv.writer(file)
+    writer.writerows(train_mask_files)
+
+with open(val_file_path, "w", newline="") as file:
+    writer = csv.writer(file)
+    writer.writerows(val_mask_files)
+
+print("Train mask file saved at:", train_file_path)
+print("Validation mask file saved at:", val_file_path)
+
+# DO SAME FOR BRAIN MASKS
+
+train_brain_mask_file = "atlas_train_brain_mask_png.csv"
+val_brain_mask_file = "atlas_val_brain_mask_png.csv"
+
+train_file_path = os.path.join(output_folder, train_brain_mask_file)
+val_file_path = os.path.join(output_folder, val_brain_mask_file)
+
+with open(train_file_path, "w", newline="") as file:
+    writer = csv.writer(file)
+    writer.writerows(train_brain_mask_files)
+
+with open(val_file_path, "w", newline="") as file:
+    writer = csv.writer(file)
+    writer.writerows(val_brain_mask_files)
+
+print("Train brain mask file saved at:", train_file_path)
+print("Validation brain mask file saved at:", val_file_path)
+
